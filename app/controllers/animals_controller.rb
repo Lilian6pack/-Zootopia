f2b0dc7c8a798a9731fcbc59ad3b8fbe8c047c6a
Lilian Besse <lilianbesse31@gmail.com>
2020-06-16 12:07:46 +0200

    Init Seed

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,42 @@
+class AnimalsController < ApplicationController
+
+  before_action :set_animal, only: [:show, :destroy]
+
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    set_animal
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animal_path(@animal)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    set_animal
+    @animal.destroy
+    redirect_to animals_path
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:user_id, :name, :description, :photo_url, :hour_price)
+  end
+
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
+
+end
