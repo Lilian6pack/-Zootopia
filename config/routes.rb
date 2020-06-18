1ffc22fa9422b146a8adebff68c6b6033b72e1df
Joseph VICENT <vicent.jsp@gmail.com>
2020-06-18 11:07:18 +0200

    Modification redirection routes home#page vers animals#index + view animal avec condition si user pas logger

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'animals/search', to: 'animals#search', as: 'search'
 
   devise_for :users
-  root to: 'pages#home'
+  root to: 'animals#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :animals, only: [ :destroy, :index, :show, :new, :create ] do 
+  resources :animals, only: [ :destroy, :index, :show, :new, :create ] do
     resources :bookings, only: [ :index, :show, :new, :create ]
   end
 end
