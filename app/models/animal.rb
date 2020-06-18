b083affc6f514fdd20444e66e7f37f0cd8b2f26c
Lilian Besse <lilianbesse31@gmail.com>
2020-06-18 11:59:47 +0200

    algolia

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,6 +3,8 @@ class Animal < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
   has_one_attached :photo_url
+  include AlgoliaSearch
+  
   # validates :name, :description, :photo_url, :hour_price, presence: true
   
 end
